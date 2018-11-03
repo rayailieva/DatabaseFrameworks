8b348c577e9b8223c57cb984558638f4c6b38f97
unknown <you@example.com>
2018-11-03 23:41:30 +0200

    new

--- 02.DBAppsIntroduction/src/app/Engine.java
+++ 02.DBAppsIntroduction/src/app/Engine.java
@@ -2,13 +2,13 @@ package app;
 
 import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class Engine implements Runnable {
 
@@ -23,46 +23,36 @@ public class Engine implements Runnable {
     @Override
     public void run() {
         try {
-            this.getVillainsNames();             // Problem 2
-
-            this.getMinionNames();               // Problem 3
-
-            //this.addMinion();                    // Problem 4
-
-            //this.changeTownNamesCasing();        // Problem 5
-
-            //this.printAllMinionNames();          // Problem 7
-
-        } catch (SQLException | IOException e) {
+            // this.getVillainsNames(); For Problem 2
+            // this.getMinionNames(); For Problem 3
+            //this.addMinion(); For Problem 4
+            //this.changeTownCasing(); For Problem 5
+            this.printNames();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
     }
 
-    /*
-    Problem 2 Get villains names
-     */
+    //Problem 2 Get Villains Names
     private void getVillainsNames() throws SQLException {
+
         String query = "SELECT v.name, COUNT(mv.minion_id) AS count FROM minions_villains AS mv JOIN villains AS v ON mv.villain_id = v.id GROUP BY villain_id HAVING count > ? ORDER BY count DESC";
 
         PreparedStatement stmt = this.connection.prepareStatement(query);
-
         stmt.setInt(1, 15);
 
         ResultSet resultSet = stmt.executeQuery();
 
-        while (resultSet.next()) {
+        while (resultSet.next()){
             System.out.println(String.format("%s %d"
                     , resultSet.getString(1)
                     , resultSet.getInt(2)));
         }
-
         connection.close();
     }
 
-    /*
-    Problem 3 Get Minion Names
-     */
-    private void getMinionNames() throws IOException, SQLException {
+    //Problem 3 Get Minion Names
+    private void getMinionNames() throws SQLException, IOException {
         int requiredVillainId = Integer.parseInt(this.reader.readLine());
 
         String query = String.format("" +
@@ -73,9 +63,8 @@ public class Engine implements Runnable {
                         "WHERE v.id = %d"
                 , requiredVillainId);
 
-        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
+        PreparedStatement stmt = this.connection.prepareStatement(query);
+        ResultSet resultSet = stmt.executeQuery();
 
         if (!resultSet.next()) {
             System.out.println(String.format("No villain with ID %d exists in the database.", requiredVillainId));
@@ -114,22 +103,20 @@ public class Engine implements Runnable {
         }
 
         System.out.println(sb);
+        connection.close();
     }
 
-    /*
-    Problem 4 Add Minion
-     */
-    private void addMinion() throws IOException, SQLException {
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+    //Problem 4 Add Minion
+    private void addMinion() throws SQLException, IOException {
 
-        String[] minionParams = reader.readLine().split("\\s+");
-        String[] villainParams = reader.readLine().split("\\s+");
+        String[] minions = reader.readLine().split(" ");
+        String[] villains = reader.readLine().split(" ");
 
-        String minionName = minionParams[1];
-        int minionAge = Integer.parseInt(minionParams[2]);
-        String townName = minionParams[3];
+        String minionName = minions[1];
+        int minionAge = Integer.parseInt(minions[2]);
+        String townName = minions[3];
 
-        String villainName = villainParams[1];
+        String villainName = villains[1];
 
         if (!this.checkIfEntityExists(townName, "towns")) {
             this.insertTown(townName);
@@ -152,8 +139,9 @@ public class Engine implements Runnable {
 
         System.out.println(String.format("Successfully added %s to be minion of %s"
                 , minionName, villainName));
-    }
 
+        connection.close();
+    }
     private boolean checkIfEntityExists(String name, String tableName) throws SQLException {
         String query = String.format("SELECT * FROM %s WHERE name = \'%s\'", tableName, name);
         PreparedStatement preparedStatement = this.connection.prepareStatement(query);
@@ -165,7 +153,6 @@ public class Engine implements Runnable {
         }
         return false;
     }
-
     private void insertTown(String townName) throws SQLException {
         String query = String.format("INSERT INTO towns(name, country) VALUES('%s', NULL)", townName);
 
@@ -173,15 +160,6 @@ public class Engine implements Runnable {
 
         preparedStatement.execute();
     }
-
-    private void insertVillain(String villainName) throws SQLException {
-        String query = String.format("INSERT INTO villains(name, evilness_factor) VALUES('%s', 'evil')", villainName);
-
-        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
-
-        preparedStatement.execute();
-    }
-
     private int getEntity(String name, String tableName) throws SQLException {
         String query = String.format("SELECT id FROM %s WHERE name = '%s'"
                 , tableName, name);
@@ -192,6 +170,14 @@ public class Engine implements Runnable {
 
         resultSet.next();
         return resultSet.getInt(1);
+    }
+
+    private void insertVillain(String villainName) throws SQLException {
+        String query = String.format("INSERT INTO villains(name, evilness_factor) VALUES('%s', 'evil')", villainName);
+
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+
+        preparedStatement.execute();
     }
 
     private void insertMinion(String minionName, int age, int townId) throws SQLException {
@@ -212,10 +198,8 @@ public class Engine implements Runnable {
         preparedStatement.execute();
     }
 
-    /*
-    Problem 5 Change Town Names Casing
-     */
-    private void changeTownNamesCasing() throws IOException, SQLException {
+    //Change Town Names Casing
+    private void changeTownCasing() throws SQLException, IOException {
         String country = reader.readLine();
 
         String query = String.format("UPDATE minions_db.towns AS t SET  t.name = UPPER (t.name) WHERE t.country = '%s'"
@@ -229,12 +213,10 @@ public class Engine implements Runnable {
                 , country);
 
         preparedStatement = this.connection.prepareStatement(query);
-
         ResultSet resultSet = preparedStatement.executeQuery();
-
         List<String> results = new ArrayList<>();
 
-        while (resultSet.next()) {
+        while (resultSet.next()){
             results.add(resultSet.getString("name"));
         }
 
@@ -244,25 +226,25 @@ public class Engine implements Runnable {
         } else {
             System.out.println("No town names were affected.");
         }
+
+        connection.close();
     }
 
-    /*
-    Problem 7 Print All Minion Names
-     */
-    private void printAllMinionNames() throws SQLException {
+    //Print All Minion Names
+    private void printNames() throws SQLException {
         String query = "SELECT name FROM minions_db.minions";
 
-        PreparedStatement pr = this.connection.prepareStatement(query);
-
-        ResultSet rs = pr.executeQuery();
+        PreparedStatement preparedStatement = this.connection.prepareStatement(query);
+        preparedStatement.execute();
+        ResultSet resultSet = preparedStatement.executeQuery();
 
         List<String> names = new ArrayList<>();
 
-        while (rs.next()) {
-            names.add(rs.getString("name"));
+        while (resultSet.next()){
+           names.add(resultSet.getString("name"));
         }
 
-        for (int i = 1; i < names.size() / 2; i += 2) {
+        for (int i = 1; i < names.size() / 2; i+=2) {
             String temp = names.get(i);
             names.set(i, names.get(names.size() - i));
             names.set(names.size() - i, temp);
@@ -273,5 +255,7 @@ public class Engine implements Runnable {
             sb.append(name).append(System.lineSeparator());
         }
         System.out.println(sb);
+
+        connection.close();
     }
 }
