0bf17464b8db7b42353992dbf4fba436d6515f71
unknown <you@example.com>
2018-11-10 10:23:43 +0200

    new

---
+++ 06.SpringDataIntro/src/main/java/bookshopsystemapp/BookshopSystemApp.java
@@ -0,0 +1,11 @@
+package bookshopsystemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookshopSystemApp {
+    public static void main(String[] args) {
+        SpringApplication.run(BookshopSystemApp.class, args);
+    }
+}
---
+++ 06.SpringDataIntro/src/main/java/bookshopsystemapp/domain/entities/Author.java
@@ -0,0 +1,31 @@
+package bookshopsystemapp.domain.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+@Entity(name = "authors")
+public class Author extends BaseEntity{
+    private String firstName;
+    private String lastName;
+
+    public Author() {
+    }
+
+    @Column(name = "first_name")
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    @Column(name = "last_name", nullable = false)
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
